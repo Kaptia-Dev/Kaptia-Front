d1589d8d2a33f5d5f97fb1847a2ccfa8888584b7
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-22 19:11:22 -0600

    feat: integrate framer-motion for animated button on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@
 
 import Header from "./components/header/Header";
 import SplitText from "./components/utils/SplitText";
+import { motion } from "framer-motion";
+
+import { ArrowDownIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-between h-screen bg-[#fffdd8]">
       <Header />
       <main className="flex flex-col flex-1 px-5 md:px-24">
-        <section className="h-screen flex flex-col md:flex-row items-center justify-center">
-          <div className="flex flex-col gap-4 w-1/2">
+        <section className="h-screen flex flex-col md:flex-row items-center justify-center relative">
+          <div className="flex flex-col gap-4 w-full md:w-1/2">
             <SplitText
               className="uppercase font-bold text-6xl md:text-8xl"
               delay={100}
@@ -35,6 +38,13 @@ export default function Home() {
               <div className="rounded-full absolute bg-custom-yellow-400/95 w-160 h-160 -left-8 -bottom-8 shadow animate-slide-in-bottom"></div>
             </div>
           </div>
+          <motion.button
+            className="rounded-full bg-custom-green-950 flex items-center justify-center absolute bottom-10 p-6 shadow cursor-pointer"
+            animate={{ y: [0, -20, 0] }}
+            transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
+          >
+            <ArrowDownIcon className="text-custom-yellow-400 h-12 w-12" />
+          </motion.button>
         </section>
         <section>
           <p>
@@ -45,7 +55,6 @@ export default function Home() {
           </p>
         </section>
       </main>
-      <footer className="bg-custom-yellow-400">Footer aquí</footer>
     </div>
   );
 }
