c38f6b6b4e524402524cc18e4fb364a143dde517
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-01-20 12:37:58 -0600

    feat: update header styles and logo for improved design consistency

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -46,18 +46,20 @@ const Header: React.FC = () => {
   return (
     <header
       className={`flex justify-center items-center w-full z-50 fixed transition-all duration-300 ${
-        scrolled ? "pt-6" : ""
+        scrolled ? "pt-5" : "md:pt-8"
       }`}
     >
       {/* Desktop Navbar */}
       <div
         className={`w-full transition-all duration-300 hidden md:flex ${
-          scrolled ? "px-5" : ""
+          scrolled ? "px-8" : "px-15"
         }`}
       >
         <nav
-          className={`items-center w-full justify-between px-5 transition-all duration-300 flex bg-[#FFFDD8]/80 backdrop-blur-[6px]  ${
-            scrolled ? "rounded-2xl py-5 shadow-xl bg-[#FFFFF0]/60" : "py-8"
+          className={`items-center w-full justify-between px-5 transition-all duration-300 flex backdrop-blur-[6px]  ${
+            scrolled
+              ? "rounded-full py-5 shadow-xl bg-secondary-blue-300/50"
+              : "py-8 bg-transparent"
           }`}
         >
           <button
@@ -67,22 +69,22 @@ const Header: React.FC = () => {
           >
             <img
               className={`${
-                scrolled ? "h-8" : "h-10"
+                scrolled ? "h-10" : "h-10"
               } transition-all duration-300`}
-              src="/KaptiaLogo.png"
+              src="/logoCombinado.webp"
               alt="Logo Kaptia"
             />
           </button>
 
           <ul
             className={`flex text-custom-green-950 items-center text-[1.2rem] ${
-              scrolled ? "gap-16" : "gap-24"
+              scrolled ? "gap-24" : "gap-14"
             } transition-all duration-300`}
           >
             {navLinks.map((link, idx) => (
               <li
                 key={idx}
-                className="animate-slideDown"
+                className="animate-slideDown uppercase text-white"
                 style={{ animationDelay: `${idx * 200}ms` }}
               >
                 <a href={link.href} className={linkClass}>
@@ -93,11 +95,11 @@ const Header: React.FC = () => {
           </ul>
 
           <button
-            className="bg-custom-yellow-400 text-custom-green-950 px-5 py-2 text-[1.2rem] rounded-lg relative font-semibold shadow cursor-pointer duration-400 overflow-hidden group"
+            className="bg-white text-primary-blue-500 px-12 py-2 text-[1.2rem] rounded-full relative font-semibold shadow cursor-pointer duration-400 overflow-hidden group"
             onClick={() => router.push("/auth")}
           >
-            <span className="absolute bg-custom-yellow-500/40 right-full top-0 bottom-0 w-full z-0 group-hover:right-0 transition-all duration-300"></span>
-            <span className="relative z-10">Iniciar Sesión</span>
+            <span className="absolute bg-custom-yellow-400 right-full top-0 bottom-0 w-full z-0 group-hover:right-0 transition-all duration-300"></span>
+            <span className="relative z-10 uppercase">Contacto</span>
           </button>
         </nav>
       </div>
@@ -112,19 +114,19 @@ const Header: React.FC = () => {
           className={`w-full flex justify-between px-5 transition-all duration-300 relative 
           ${
             scrolled
-              ? `rounded-2xl backdrop-blur-[6px] shadow-xl py-5 bg-[#F9FCED]/70`
+              ? `rounded-2xl backdrop-blur-[6px] shadow-xl py-5 bg-secondary-blue-300/50`
               : `py-8 bg-transparent`
           }`}
         >
           <button className="transition-all duration-300 hover:scale-[1.05] group">
-            <UserCircleIcon className="h-8 w-8 text-custom-green-950 cursor-pointer" />
+            <UserCircleIcon className="h-8 w-8 text-white cursor-pointer" />
           </button>
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
             className="cursor-pointer"
             aria-label="Ir al inicio"
           >
-            <img className="h-8" src="/KaptiaLogo.png" alt="Logo Kaptia" />
+            <img className="h-8" src="/logoCombinado.webp" alt="Logo Kaptia" />
           </button>
           <button
             className="transition-all duration-300 hover:scale-[1.05] relative w-8 h-8"
@@ -135,14 +137,14 @@ const Header: React.FC = () => {
                 ${menuOpen ? "opacity-0 scale-90" : "opacity-100 scale-100"}
               `}
             >
-              <Bars3Icon className="h-8 w-8 text-custom-green-950 cursor-pointer" />
+              <Bars3Icon className="h-8 w-8 text-white cursor-pointer" />
             </span>
             <span
               className={`absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out
                 ${menuOpen ? "opacity-100 scale-100" : "opacity-0 scale-90"}
               `}
             >
-              <XMarkIcon className="h-8 w-8 text-custom-green-950 cursor-pointer" />
+              <XMarkIcon className="h-8 w-8 text-white cursor-pointer" />
             </span>
           </button>
 
@@ -151,8 +153,8 @@ const Header: React.FC = () => {
               <ul
                 className={`shadow-xl py-5 bg-custom-yellow-400 text-center text-custom-green-950 text-[1.2rem] flex flex-col gap-4 transition-all duration-300
               ${menuOpen ? "animate-slideDown" : "animate-slideUp"} ${
-                  scrolled ? "rounded-2xl" : ""
-                }
+                scrolled ? "rounded-2xl" : ""
+              }
               `}
               >
                 {navLinks.map((link, idx) => (
