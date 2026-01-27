d69bd8a95e43400f7f03eaa585aa8546679f1781
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2026-01-27 11:12:10 -0600

    feat: enhance landing page design with updated text, button styles, and new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,14 @@ import { ArrowDownIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col justify-between bg-primary-blue-500">
+    <div className="h-full flex flex-col justify-between">
       <Header />
-      <main className="flex flex-col flex-1 px-5 md:px-24">
-        <section className="h-screen flex flex-col md:flex-row items-center justify-center relative">
-          <div className="flex flex-col gap-4 w-full md:w-1/2">
+      <main className="flex flex-col flex-1">
+        {/* Hero */}
+        <section className="h-screen flex flex-col md:flex-row items-center justify-center relative bg-primary-blue-500 px-5 md:px-24">
+          <div className="flex flex-col gap-10 w-full md:w-1/2 z-20">
             <SplitText
-              className="uppercase font-bold text-6xl md:text-8xl"
+              className="font-bold text-4xl md:text-7xl text-white"
               delay={100}
               duration={0.6}
               ease="power3.out"
@@ -25,48 +26,107 @@ export default function Home() {
               rootMargin="-100px"
               textAlign="left"
             >
-              Datos que <span className="text-custom-yellow-400">cultivan</span>{" "}
-              ventas
+              Encuentra a tus clientes antes que tu competencia
             </SplitText>
-            <span className="text-[1.2rem] md:text-2xl animate-slide-in-left">
-              Tu plataforma integral para la gestión y optimización de recursos
-              agrícolas
+            <span className="text-[1.2rem] md:text-2xl animate-slide-in-left text-white">
+              Kaptia es una plataforma inteligente para equipos de ventas que
+              combina prospección geolocalizada, CRM y seguimiento con
+              inteligencia artificial.
             </span>
-          </div>
-          <div className="w-1/2 hidden md:block">
-            <div className="rounded-full bg-custom-green-950 h-200 w-200 relative shadow animate-slide-in-right">
-              <div className="rounded-full absolute bg-custom-yellow-400/95 w-160 h-160 -left-8 -bottom-8 shadow animate-slide-in-bottom"></div>
+            <div>
+              <button className="bg-custom-yellow-400 text-primary-blue-500 px-12 py-4 text-[1.2rem] md:text-2xl rounded-full relative font-semibold shadow cursor-pointer duration-400 overflow-hidden group">
+                <span className="absolute bg-white right-full top-0 bottom-0 w-full z-0 group-hover:right-0 transition-all duration-300"></span>
+                <span className="relative z-10 uppercase">Solicitar Demo</span>
+              </button>
             </div>
           </div>
+          <div className="hidden md:w-1/2 md:flex md:justify-end">
+            <img
+              className="w-130 animate-slide-in-right"
+              src="./figura_1.png"
+              alt="Rectangulo decorativo"
+            />
+          </div>
           <motion.button
-            className="rounded-full bg-custom-green-950 flex items-center justify-center absolute bottom-10 p-6 shadow cursor-pointer"
+            className="rounded-full bg-secondary-blue-300 flex items-center justify-center absolute bottom-10 p-6 shadow cursor-pointer group hover:bg-custom-yellow-400 transition-colors duration-300 z-100"
             animate={{ y: [0, -20, 0] }}
             transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
-            onClick={() => {
-              const section = document.getElementById("sobre-nosotros");
-              if (section) {
-                section.scrollIntoView({ behavior: "smooth" });
-              }
-            }}
+            onClick={() =>
+              window.scrollBy({ top: window.innerHeight, behavior: "smooth" })
+            }
           >
-            <ArrowDownIcon className="text-custom-yellow-400 h-12 w-12" />
+            <ArrowDownIcon className="text-custom-yellow-400 h-12 w-12 group-hover:text-primary-blue-500 transition-colors duration-300" />
           </motion.button>
         </section>
-        <section
-          className="min-h-screen flex flex-col-reverse md:flex-row items-center justify-center px-4 md:px-0 py-10 md:py-0 gap-8 md:gap-0"
-          id="sobre-nosotros"
-        >
-          <div className="w-full md:w-1/2 flex flex-col gap-6 md:gap-10 items-center md:items-start text-center md:text-left">
-            <h2 className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-bold text-custom-green-950">
-              ¿Quiénes Somos?
-            </h2>
-            <p className="text-custom-green-950 text-base sm:text-lg md:text-2xl max-w-xl">
-              Tu mejor opción de agente inteligente para impulsar la venta de
-              fertilizantes orgánicos a base de sargazo. Utilizamos datos y
-              análisis inteligente para prospectar clientes y hacer seguimiento
-              a cada venta.
+
+        {/* Section 01 */}
+        <section className="h-screen bg-primary-blue-500 px-5 md:px-24 flex flex-col items-center justify-center">
+          <div className="flex flex-col justify-center items-center gap-24">
+            <img
+              className="h-32 animate-slide-in-top"
+              src="./figura_7.png"
+              alt=""
+            />
+            <p className="text-2xl md:text-5xl text-white font-extralight text-center animate-slide-in-top">
+              La prospección es la{" "}
+              <strong className="font-extrabold text-4xl md:text-6xl">
+                2°
+              </strong>{" "}
+              actividad que{" "}
+              <span className="uppercase font-bold text-secondary-blue-300">
+                <br />
+                más tiempo consume
+              </span>
+              <span className="uppercase font-bold text-yellow-400">
+                {" "}
+                <br />
+                en el proceso de ventas
+              </span>
             </p>
+            <div className="flex flex-col md:flex-row gap-10">
+              <div className="animate-slide-in-left">
+                <p className="font-extralight text-2xl md:text-5xl text-white">
+                  Menos del{" "}
+                  <strong className="font-extrabold text-secondary-blue-300 text-4xl md:text-6xl">
+                    2%
+                  </strong>{" "}
+                  <br />
+                  de las empresas <br />
+                  productoras en{" "}
+                  <span className="font-bold text-yellow-400">México</span>
+                  <span className="font-bold text-secondary-blue-300">
+                    {" "}
+                    <br />
+                    utilizan plataformas digitales{" "}
+                  </span>
+                  <span className="font-bold text-yellow-400">
+                    <br />
+                    para gestión comercial
+                  </span>
+                </p>
+              </div>
+              <div className="animate-slide-in-right">
+                <img
+                  className="h-72"
+                  src="./figura_6.png"
+                  alt="Figuras con un mapa de la república mexicana"
+                />
+              </div>
+            </div>
           </div>
+        </section>
+
+        {/* Section 02 */}
+        <section className="h-screen bg-white px-5 md:px-24 items-center justify-center flex flex-col gap-10">
+          <span className="text-secondary-blue-300 font-extralight text-center text-2xl md:text-5xl">Así nace</span>
+          <img src="./logoAzul.webp" alt="Logo Azul de Kaptia" />
+          <p className="text-primary-blue-500 font-extrabold text-center text-2xl md:text-5xl">
+            Un sistema inteligente de prospección y <br />
+            gestión comercial diseñada para hacer el <br />
+            proceso de ventas más <br />
+            <span className="text-yellow-400 italic">fácil y rápido</span>
+          </p>
+          <img src="./clock.png" alt="Imagen de un reloj" />
         </section>
       </main>
     </div>
