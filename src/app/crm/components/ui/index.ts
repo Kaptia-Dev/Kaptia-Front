ea64cf6eb37761d3fb85a16a62cd01bde936efe1
Leo-Noriega <leonoriega100@gmail.com>
2026-01-28 11:15:58 -0600

    feat: update index file to export new UI components: SearchBar, ReminderCard, FunnelHealthCard, and ChartCard for streamlined imports

--- src/app/crm/components/ui/index.ts
+++ src/app/crm/components/ui/index.ts
@@ -2,3 +2,7 @@ export { StatCard } from "./StatCard";
 export { ProgressBar } from "./ProgressBar";
 export { AlertCard } from "./AlertCard";
 export { MetricCard } from "./MetricCard";
+export { SearchBar } from "./SearchBar";
+export { ReminderCard } from "./ReminderCard";
+export { FunnelHealthCard } from "./FunnelHealthCard";
+export { ChartCard } from "./ChartCard";
